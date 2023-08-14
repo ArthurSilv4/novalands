17934f2e49d42df1bd0678c040b12811e2ed88f7
Arthur De Souza <arthurdesouza.silv@gmail.com>
2023-08-13 21:22:10 -0300

    fixed the alet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
-import { Alert } from 'components/Alert/Alert'
+'use client'
+
 import { SectionCommunity } from 'components/sections/sectionCommunity/sectionCommunity'
 import { SectionGameInfo } from 'components/sections/sectionGameInfo/sectionGameInfo'
 import { SectionHome } from 'components/sections/sectionHome/sectionHome'
 import { SectionNews } from 'components/sections/sectionNews/sectionNews'
 import { SectionSocials } from 'components/sections/sectionSocials/sectionSocials'
+import { useEffect } from 'react'
 
 export default function Home() {
+  const Alert = () => {
+    alert('Esse site não é o oficial, é apenas um site feito por fã')
+  }
+
+  useEffect(() => {
+    Alert()
+  }, [])
+
   return (
     <main>
-      <Alert />
       <SectionHome />
       <SectionGameInfo />
       <SectionNews />
