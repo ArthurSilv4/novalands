23a5fda8b8d9d81fac741c3d2eaba91e5840dd49
Arthur de Souza Silva <arthurdesouza.silv@gmail.com>
2023-08-29 16:41:05 -0300

    finalizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
-'use client'
-
 import { SectionCommunity } from 'components/sections/sectionCommunity/sectionCommunity'
 import { SectionGameInfo } from 'components/sections/sectionGameInfo/sectionGameInfo'
 import { SectionHome } from 'components/sections/sectionHome/sectionHome'
 import { SectionNews } from 'components/sections/sectionNews/sectionNews'
 import { SectionSocials } from 'components/sections/sectionSocials/sectionSocials'
-import { useEffect } from 'react'
 
 export default function Home() {
-  const Alert = () => {
-    alert('Esse site não é o oficial, é apenas um site feito por fã')
-  }
-
-  useEffect(() => {
-    Alert()
-  }, [])
-
   return (
     <main>
       <SectionHome />
@@ -23,10 +12,6 @@ export default function Home() {
       <SectionNews />
       <SectionSocials />
       <SectionCommunity />
-
-
-
-
     </main>
   )
 }
